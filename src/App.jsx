cb49853b1366d0b5435fe9d4db0a660a8ffcb964
Andre Dias <andredias.curitiba@gmail.com>
2024-03-24 09:12:46 -0300

    subindo

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React - Projeto Senai CRUD</h1>
+      <div>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <div>
+          <h2>Login</h2>
+          <form action="">
+            <div>
+              <label htmlFor="name">Nome:</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="password">Senha:</label>
+              <input type="password" />
+            </div>
+            <button>Entrar</button>
+          </form>
+        </div>
+
+      </div>
+    
+    </>
+  )
+}
+
+export default App
